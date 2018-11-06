2c343967481ce7b81d5bb05f114ad5358b508360
Chuck Hilyard <chuck.hilyard@reachlocal.com>
2018-11-06 14:29:57 -0800

    with inotify

--- cron.sh
+++ cron.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-current_date=$(date +%m-%d-%Y)
-echo "current date - $current_date"
- users=(`ls -1 /home`)
- for user in "${users[@]}"
-do
-   echo "user - $user"
-   if [ ! -d "/home/$user/catalog/$current_date" ]; then ###Checking if current date directory exists
-      mkdir -p /home/$user/catalog/$current_date
-   fi
-    for feed_file in /home/$user/catalog/*$current_date*.csv
-    do
-        echo "feed file - $feed_file"
-        cp $feed_file /home/$user/catalog/$current_date/
-    done
-done
+modified_file_name_path="$@" #Should be injected by the inotify listener
+
+#Sample file path
+#modified_file_name_path="/home/testuser/catalog/Dealer123_USA_11-01-2018_15_21_20.csv"
+
+file_name_date=$(expr "$modified_file_name_path" : '.*/\([^/]*\).*' | cut -f 3 -d "_")
+echo $file_name_date
+
+user_dir=$(echo "$modified_file_name_path" | cut -f 3 -d "/")
+echo $user_dir
+
+ if [ ! -d "/home/$user_dir/catalog/$file_name_date" ]; then ###Checking if current date directory exists
+      mkdir -p /home/$user_dir/catalog/$file_name_date
+ fi
+
+cp $modified_file_name_path /home/$user_dir/catalog/$file_name_date/
