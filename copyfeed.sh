ed64a5479b564cca3c29077bc81bdf7cf208b531
VimalG <vimal.gopalakrishnan@reachlocal.com>
2018-11-08 15:13:57 -0800

    [DATE LOG FILES AND TIMESTAMP IN THE LOGS]

--- copyfeed.sh
+++ copyfeed.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Log Location on Server.
+currentdate=`date "+%Y-%m-%d %H:%M:%S"`
+
 LOG_LOCATION=/rl/data/logs/facebook-auto-feed/
-exec >> $LOG_LOCATION/copyfeed.log 2>&1
+exec >> $LOG_LOCATION/copyfeed-$currentdate.log 2>&1
 
 echo "*******  Start of copyfeed.sh script *******"
 
@@ -10,26 +12,26 @@ inotifywait -m /home/testuser/catalog/ -e create -e modify |
 while read path action file;
 
    do
-      LOGTIME=`date "+%Y-%m-%d %H:%M:%S"`
-      echo $LOGTIME": The file '$file' appeared in directory '$path' via '$action'"
+      timestamp=`date "+%Y-%m-%d %H:%M:%S"`
+      echo "$timestamp: The file '$file' appeared in directory '$path' via '$action'"
 
 
       sourceFilePath="$path$file" #Should be injected by the inotify listener
 
-      echo "sourceFilePath : $sourceFilePath"
+      echo "$timestamp: sourceFilePath : $sourceFilePath"
       #Sample file path
       #sourceFilePath="/home/testuser/catalog/Dealer123_USA_11-01-2018_15_21_20.csv"
 
       dateInSourceFilePath=$(expr "$sourceFilePath" : '.*/\([^/]*\).*' | cut -f 3 -d "_")
-      echo "dateInSourceFilePath : $dateInSourceFilePath"
+      echo "$timestamp: dateInSourceFilePath : $dateInSourceFilePath"
 
       #Format date to yyyy-mm-dd
       IFS='-' mmddyyyy=(${dateInSourceFilePath})
       yyyymmdd="${mmddyyyy[2]}-${mmddyyyy[0]}-${mmddyyyy[1]}"
-      echo "yyyymmdd : $yyyymmdd"
+      echo "$timestamp: yyyymmdd : $yyyymmdd"
 
      user_dir=$(echo "$sourceFilePath" | cut -f 3 -d "/")
-     echo "user_dir : $user_dir"
+     echo "$timestamp: user_dir : $user_dir"
 
      targetDir="/rl/data/autofeed/$yyyymmdd"
 
@@ -38,6 +40,6 @@ while read path action file;
      fi
 
      cp $sourceFilePath $targetDir
-     echo "***** SUCCESS : Copied $sourceFilePath to $targetDir *****"
+     echo "$timestamp: ***** SUCCESS : Copied $sourceFilePath to $targetDir *****"
 
 done
