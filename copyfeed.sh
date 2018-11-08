8f0f8dd88df63a5d7d3b63e7b40aea81e423d045
vsunkari <vsunkari@gannett.com>
2018-11-08 14:14:43 -0800

    DISP-3197 : inotify shell script to copy csv feeds from user/catalog directory to date directory

--- copyfeed.sh
+++ copyfeed.sh
@@ -1,55 +1,45 @@
 #!/bin/bash
+# Log Location on Server.
+LOG_LOCATION=/rl/data/logs/facebook-auto-feed/
+exec >> $LOG_LOCATION/copyfeed.log 2>&1
 
 echo "*******  Start of copyfeed.sh script *******"
 
-# Log Location on Server.
-LOG_LOCATION=/rl/data/logs/facebook-auto-feed/
-CURRENTDATE=`date "+%Y-%m-%d"`
-exec >> $LOG_LOCATION/copyfeed_$CURRENTDATE.log 2>&1
-
-# inotify monitoring 2 dirs:
-# /home/testuser/catalog
-# /home/testuser2/catalog
-inotifywait -m /home/testuser/catalog/ /home/testuser2/catalog -e create -e modify |
+inotifywait -m /home/testuser/catalog/ -e create -e modify |
 
 while read path action file;
 
    do
       LOGTIME=`date "+%Y-%m-%d %H:%M:%S"`
-      echo "$LOGTIME: The file '$file' appeared in directory '$path' via '$action'"
+      echo $LOGTIME": The file '$file' appeared in directory '$path' via '$action'"
 
 
-      modified_file_name_path="$path$file" #Should be injected by the inotify listener
+      sourceFilePath="$path$file" #Should be injected by the inotify listener
 
-      echo "$LOGTIME: modified_file_name_path : $modified_file_name_path"
+      echo "sourceFilePath : $sourceFilePath"
       #Sample file path
-      #modified_file_name_path="/home/testuser/catalog/Dealer123_USA_11-01-2018_15_21_20.csv"
+      #sourceFilePath="/home/testuser/catalog/Dealer123_USA_11-01-2018_15_21_20.csv"
 
-      file_name_date=$(expr "$modified_file_name_path" : '.*/\([^/]*\).*' | cut -f 3 -d "_")
-      echo "$LOGTIME: file_name_date : $file_name_date"
+      dateInSourceFilePath=$(expr "$sourceFilePath" : '.*/\([^/]*\).*' | cut -f 3 -d "_")
+      echo "dateInSourceFilePath : $dateInSourceFilePath"
 
-      if [ ${#file_name_date} -ne 10 ]; then
-              echo "$LOGTIME: Incorrect length of file_name_date : ${file_name_date}"
-              exit 1
-      fi
+      #Format date to yyyy-mm-dd
+      IFS='-' mmddyyyy=(${dateInSourceFilePath})
+      yyyymmdd="${mmddyyyy[2]}-${mmddyyyy[0]}-${mmddyyyy[1]}"
+      echo "yyyymmdd : $yyyymmdd"
 
+     user_dir=$(echo "$sourceFilePath" | cut -f 3 -d "/")
+     echo "user_dir : $user_dir"
 
-      yyyymmdd="${file_name_date:6:4}-${file_name_date:0:2}-${file_name_date:3:2}"
+     targetDir="/rl/data/autofeed/$yyyymmdd"
 
-      #user_dir=$(echo "$modified_file_name_path" | cut -f 3 -d "/")
-      #echo "user_dir : $user_dir"
+     if [ ! -d $targetDir ]; then ###Checking if current date directory exists
+       mkdir -p $targetDir
+     fi
 
-      if [ ! -d "/rl/data/feed/$yyyymmdd" ]; then ###Checking if current date directory exists
-          mkdir -p /rl/data/feed/$yyyymmdd
-          echo "$LOGTIME: Made dir /rl/data/feed/$yyyymmdd"
-      else
-          echo "$LOGTIME: Dir /rl/data/feed/$yyyymmdd  already exists"
-      fi
-
-      if cp $modified_file_name_path /rl/data/feed/$yyyymmdd/; then
-          echo "$LOGTIME: ******* Copy Code: $? - SUCCESS Copying $modified_file_name_path to /rl/data/feed/$yyyymmdd/  *******"
-      else
-          echo "$LOGTIME: ******* Copy Code: $? - FAILED Copying $modified_file_name_path to /rl/data/feed/$yyyymmdd/   *******"
-      fi
+     cp $sourceFilePath $targetDir
+     echo "***** SUCCESS : Copied $sourceFilePath to $targetDir *****"
 
 done
+
+>> LOG_LOCATION/copyfeed.log
