9f10fb4bde3d2ab834309e4dae567bb8b1fcfcb0
vsunkari <vsunkari@gannett.com>
2018-11-16 13:49:30 -0800

    Disp 3197 (#5)
    * DISP-3197 : inotify shell script to copy csv feeds from user/catalog directory to date directory
    
    * DISP-3197 : inotify shell script to copy csv feeds from user/catalog directory to date directory
    
    * [DATE LOG FILES AND TIMESTAMP IN THE LOGS]
    
    * [DATE LOG FILES AND TIMESTAMP IN THE LOGS]
    
    * DISP-3197 : inotify shell script to copy csv feeds from user/catalog directory to date directory - 2
    
    * DISP-3197 : inotify shell script to copy csv feeds from user/catalog directory to date directory - 2
    
    * DISP-3197 : inotify shell script to copy csv feeds from user/catalog directory to date directory - 2
    
    * DISP-3197 : inotify shell script to copy csv feeds from user/catalog directory to date directory - 2
    
    * DISP-3197 : inotify shell script to copy csv feeds from user/catalog directory to date directory - 2
    
    * DISP-3197 : inotify shell script to copy csv feeds from user/catalog directory to date directory - 2
    
    * DISP-3197 : inotify shell script to copy csv feeds from user/catalog directory to date directory - 3
    
    * DISP-3197 : inotify shell script to copy csv feeds from user/catalog directory to date directory - 3
    
    * Revert "DISP-3197 : inotify shell script to copy csv feeds from user/catalog directory to date directory - 3"
    
    This reverts commit 8868c944ee28436a3e5f00c4ba0579d65ae6d470.
    
    * "DISP-3197 : inotify shell script to copy csv feeds from user/catalog directory to date directory - 3"
    
    * "DISP-3197 : inotify shell script to copy csv feeds from user/catalog directory to date directory - 3"
    
    * "DISP-3197 : inotify shell script to copy csv feeds from user/catalog directory to date directory - 3"
    
    * "DISP-3197 : crontab config for copyfeed"
    
    * "DISP-3197 : crontab config for copyfeed"

--- copyfeed.sh
+++ copyfeed.sh
@@ -1,55 +1,63 @@
 #!/bin/bash
 
-echo "*******  Start of copyfeed.sh script *******"
+LOGTIME=`date "+%Y-%m-%d %H:%M:%S"`
+echo "$LOGTIME: Pinging copyfeed .. "
 
 # Log Location on Server.
 LOG_LOCATION=/rl/data/logs/facebook-auto-feed/
 CURRENTDATE=`date "+%Y-%m-%d"`
 exec >> $LOG_LOCATION/copyfeed_$CURRENTDATE.log 2>&1
 
-# inotify monitoring 2 dirs:
-# /home/testuser/catalog
-# /home/testuser2/catalog
-inotifywait -m /home/testuser/catalog/ /home/testuser2/catalog -e create -e modify |
+echo "$LOGTIME: Pinging copyfeed process .. "
+PROCESS_ID=$(ps -ef | grep inotifywait | grep -v grep | grep -v \<defunct\> | awk '{print $2}')
+echo "PROCESS_ID : $PROCESS_ID"
+if [[ ! -z "$PROCESS_ID" ]]; then
+    echo "$LOGTIME: copyfeed process already running"
+else
+    echo "$LOGTIME: copyfeed process NOT running.  Initating a new one"
+    # inotify monitoring 2 dirs:
+    # /home/vautoreachlocal/catalog
+    # /home/homenetautoreachlocal/catalog
+    inotifywait -m /home/vautoreachlocal/catalog/ /home/homenetautoreachlocal/catalog -e create -e modify |
 
-while read path action file;
+    while read path action file;
 
-   do
-      LOGTIME=`date "+%Y-%m-%d %H:%M:%S"`
-      echo "$LOGTIME: The file '$file' appeared in directory '$path' via '$action'"
+       do
+          LOGTIME=`date "+%Y-%m-%d %H:%M:%S"`
+          echo "$LOGTIME: The file '$file' appeared in directory '$path' via '$action'"
 
+          #Sample file path
+          #modified_file_name_path="/home/testuser/catalog/Dealer123_2018-11-10_15:21:20.csv"
+          modified_file_name_path="$path$file" #Should be injected by the inotify listener
 
-      modified_file_name_path="$path$file" #Should be injected by the inotify listener
+          echo "$LOGTIME: modified_file_name_path : $modified_file_name_path"
 
-      echo "$LOGTIME: modified_file_name_path : $modified_file_name_path"
-      #Sample file path
-      #modified_file_name_path="/home/testuser/catalog/Dealer123_USA_11-01-2018_15_21_20.csv"
+          file_name_date=$(expr "$modified_file_name_path" : '.*/\([^/]*\).*' | cut -f 2 -d "_")
+          echo "$LOGTIME: file_name_date : $file_name_date"
 
-      file_name_date=$(expr "$modified_file_name_path" : '.*/\([^/]*\).*' | cut -f 3 -d "_")
-      echo "$LOGTIME: file_name_date : $file_name_date"
+          if [ ${#file_name_date} -ne 10 ]; then
+                  echo "$LOGTIME: Incorrect length of file_name_date : ${file_name_date}"
+                  exit 1
+          fi
 
-      if [ ${#file_name_date} -ne 10 ]; then
-              echo "$LOGTIME: Incorrect length of file_name_date : ${file_name_date}"
-              exit 1
-      fi
+          #To convert date format if needed
+          #yyyymmdd="${file_name_date:6:4}-${file_name_date:0:2}-${file_name_date:3:2}"
 
+          yyyymmdd=${file_name_date}
 
-      yyyymmdd="${file_name_date:6:4}-${file_name_date:0:2}-${file_name_date:3:2}"
+          if [ ! -d "/rl/data/feed/$yyyymmdd" ]; then ###Checking if current date directory exists
+              mkdir -p /rl/data/feed/$yyyymmdd
+              echo "$LOGTIME: Made dir /rl/data/feed/$yyyymmdd"
+          else
+              echo "$LOGTIME: Dir /rl/data/feed/$yyyymmdd  already exists"
+          fi
 
-      #user_dir=$(echo "$modified_file_name_path" | cut -f 3 -d "/")
-      #echo "user_dir : $user_dir"
+          if cp $modified_file_name_path /rl/data/feed/$yyyymmdd/; then
+              echo "$LOGTIME: ******* Copy Code: $? - SUCCESS Copying $modified_file_name_path to /rl/data/feed/$yyyymmdd/  *******"
+          else
+              echo "$LOGTIME: ******* Copy Code: $? - FAILED Copying $modified_file_name_path to /rl/data/feed/$yyyymmdd/   *******"
+          fi
 
-      if [ ! -d "/rl/data/feed/$yyyymmdd" ]; then ###Checking if current date directory exists
-          mkdir -p /rl/data/feed/$yyyymmdd
-          echo "$LOGTIME: Made dir /rl/data/feed/$yyyymmdd"
-      else
-          echo "$LOGTIME: Dir /rl/data/feed/$yyyymmdd  already exists"
-      fi
+    done
 
-      if cp $modified_file_name_path /rl/data/feed/$yyyymmdd/; then
-          echo "$LOGTIME: ******* Copy Code: $? - SUCCESS Copying $modified_file_name_path to /rl/data/feed/$yyyymmdd/  *******"
-      else
-          echo "$LOGTIME: ******* Copy Code: $? - FAILED Copying $modified_file_name_path to /rl/data/feed/$yyyymmdd/   *******"
-      fi
-
-done
+fi
